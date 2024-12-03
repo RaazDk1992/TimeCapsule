78331e532c63f3675e044a153ccbefecd49beeed
RaazDk1992 <dhakal1992raaz@gmail.com>
2024-12-03 15:36:33 +0545

    timecapsule init

---
+++ src/main/java/com/raazdk/TimeCapsule/TimeCapsuleApplication.java
@@ -0,0 +1,13 @@
+package com.raazdk.TimeCapsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeCapsuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeCapsuleApplication.class, args);
+	}
+
+}
